da96a836178c3008dd59cbf470cf3e722933c49e
ashonweb <meghpalbhagya@gmail.com>
2019-04-06 00:45:02 +0530

    checking route functionality

---
+++ src/Contentpage.js
@@ -0,0 +1,11 @@
+import React,{Component } from 'react';
+
+export default class Contentpage extends Component {
+  render(){
+    return(
+      <div>
+       <p>dkajdkdjkajdkdj</p>
+      </div>
+    )
+  }
+}
---
+++ src/New.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
+
+export default class Navigationnew extends Component {
+ 
+  
+  render(){
+    return (
+      <div>
+        <div className="topnav" id="myTopnav">
+
+       
+        <Link to="/contentpage">ABOUT</Link>
+     </div>
+     </div>
+    )}
+}
